a8f64f16f6e33dc49e0be7183ca703bd22afb20e
daniel <dphilibert@weka-fachmedien.de>
2013-04-05 14:37:50 +0200

    autofocus fuer Suchformular im Admin-Bereich, Icon fuer Admin-Funktion: zum eignen Anbieter wechseln bei der Infoleiste oben

--- application/forms/Search.php
+++ application/forms/Search.php
@@ -1,25 +1,27 @@
 <?php
 
+/**
+ * Suchformular für das Admin-Modul
+ *  
+ */
 class Form_Search extends Zend_Form
 {
   var $action = '/admin/index/index';
   var $name = 'search';
-  var $method = 'POST';
-  
+  var $method = 'POST';  
   var $decorators = array (
     array ('HtmlTag', array ('tag' => 'div', 'style' => 'float:left;margin-right:20px;')),   
   );
         
   public function init ()
   {
-    $this->setAction ($this->action)->setName ($this->name)->setMethod ($this->method);
+    $this->setAction ($this->action)->setName ($this->name)->setMethod ($this->method);    
     
     $search = new Zend_Form_Element_Text ('search_term');
-    $search->setAttrib ('class', 'searchBox')->setAttrib ('placeholder', 'Suche Name/ID')->addDecorators ($this->decorators)->removeDecorator ('Label');
-                    
-    $this->addElements (array (
-       $search       
-    ));
+    $search->setAttribs (array ('class' => 'searchBox', 'placeholder' => 'Suche Name/ID', 'autofocus' => 'true'))
+            ->addDecorators ($this->decorators)->removeDecorator ('Label');
+    
+    $this->addElement ($search);
   }        
   
 }
